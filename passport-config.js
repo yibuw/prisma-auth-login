9f969e8dfc336617e52220df9afde1ef9dc58a6e
name <email>
2021-09-17 14:27:46 +0800

    update code

--- passport-config.js
+++ passport-config.js
@@ -1,29 +1,49 @@
-const LocalStrategy = require('passport-local').Strategy
-const bcrypt = require('bcrypt')
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
-function initialize(passport, getUserByEmail, getUserById) {
+function initialize(passport) {
   const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email)
+    // console.log(
+    //   '🚀 ~ file: passport-config.js ~ line 6 ~ authenticateUser ~ email, password, done',
+    //   email,
+    //   password,
+    //   done
+    // );
+    // const user = getUserByEmail(email);
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
     if (user == null) {
-      return done(null, false, { message: 'No user with that email' })
+      return done(null, false, { message: 'No user with that email' });
     }
 
     try {
       if (await bcrypt.compare(password, user.password)) {
-        return done(null, user)
+        return done(null, user);
       } else {
-        return done(null, false, { message: 'Password incorrect' })
+        return done(null, false, { message: 'Password incorrect' });
       }
     } catch (e) {
-      return done(e)
+      return done(e);
     }
-  }
+  };
 
-  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser))
-  passport.serializeUser((user, done) => done(null, user.id))
-  passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id))
-  })
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser(async (id, done) => {
+    const res = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    return done(null, res);
+  });
 }
 
-module.exports = initialize
+module.exports = initialize;
